925da43fe4d969592e8e6f29e0a2cda4ed5f696d
alopez244 <alejandro.lopez@ehu.eus>
2020-03-17 17:55:21 +0100

    cmd files setup ok

--- 3_startprocnode.cmd
+++ 3_startprocnode.cmd
@@ -1,4 +1,1 @@
-:inicio
-java -Dlog4j.configurationFile=file:log4j2.xml -cp bin;lib\log4j-api-2.3.jar;lib\log4j-core-2.3.jar;log4j-api-2.3.jar;lib\log4j-core-2.3.jar;lib\commons-codec-1.3.jar jade.Boot -container reg-%random%:es.ehu.platform.agents.ProcNodeAgent("System=system","description=description","refServID=id55")
-TIMEOUT /T 4
-goto inicio
+java -Dlog4j.configurationFile="log4j2.xml" -cp ".\lib\jade;.\lib\log4j\log4j-api-2.3.jar;.\lib\log4j\log4j-core-2.3.jar;.\lib\commons\commons-codec-1.3.jar;.\lib\commons\commons-io-2.6.jar;.\classes" jade.Boot -container reg-%random%:es.ehu.platform.agents.ProcNodeAgent("System=system","description=description","refServID=id55")
