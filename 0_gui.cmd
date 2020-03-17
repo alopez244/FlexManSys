8c30b7542d69c0d59d8ddbe1e2086f389a01ba2f
alopez244 <alejandro.lopez@ehu.eus>
2020-03-17 13:52:50 +0100

    More Changes on 17/03/2020

--- 0_gui.cmd
+++ 0_gui.cmd
@@ -1,1 +1,1 @@
-java -cp ".\lib\jade;.\lib\log4j\log4j-api-2.3.jar;.\lib\log4j\log4j-core-2.3.jar" -Dlog4j.configurationFile="log4j2.xml"  jade.Boot -gui
+java -Dlog4j.configurationFile="log4j2.xml" -cp ".\lib\jade;.\lib\log4j\log4j-api-2.3.jar;.\lib\log4j\log4j-core-2.3.jar" jade.Boot -gui
--- 1_sa.cmd
+++ 1_sa.cmd
@@ -1,2 +1,1 @@
-cd .\classes
-java -Dlog4j.configurationFile="log4j2.xml" -cp "..\lib\jade;..\lib\log4j\log4j-api-2.3.jar;..\lib\log4j\log4j-core-2.3.jar;..\lib\commons\commons-codec-1.3.jar;..\lib\commons\commons-io-2.6.jar" jade.Boot -container sa:es.ehu.SystemModelAgent
+java -Dlog4j.configurationFile="log4j2.xml" -cp ".\lib\jade;.\lib\log4j\log4j-api-2.3.jar;.\lib\log4j\log4j-core-2.3.jar;.\lib\commons\commons-codec-1.3.jar;.\lib\commons\commons-io-2.6.jar;.\classes" jade.Boot -container sa:es.ehu.SystemModelAgent
