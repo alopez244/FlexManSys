44d1ae7ea6c64b50d7e8f450afbca1e1a494bde6
alopez244 <alejandro.lopez@ehu.eus>
2020-03-30 13:27:57 +0200

    30/03/2020 Updated cmd files.

---
+++ 32_startprocnode2.cmd
@@ -0,0 +1,1 @@
+java -Dlog4j.configurationFile="log4j2.xml" -cp ".\lib\jade:.\lib\log4j\log4j-api-2.3.jar:.\lib\log4j\log4j-core-2.3.jar:.\lib\commons\commons-codec-1.3.jar:.\lib\commons\commons-io-2.6.jar:.\classes" jade.Boot -container -host 192.168.0.12 reg-%random%:es.ehu.platform.agents.ProcNodeAgent("System=system","description=description","refServID=id57")
