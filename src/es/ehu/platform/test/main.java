2b8a1d6328f0b29a267f81a14ec0022a6b807c77
alopez244 <alejandro.lopez@ehu.eus>
2020-04-13 11:00:22 +0200

    12/04/2020 mesing aroung with the XMLReading.

--- src/es/ehu/platform/test/main.java
+++ src/es/ehu/platform/test/main.java
@@ -2,9 +2,18 @@ package es.ehu.platform.test;
 
 import es.ehu.platform.utilities.XMLReader;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) throws Exception {
-        String uri="classes/resources/AppInstances/MP1.xml";
+        String appPath="classes/resources/AppInstances/";
+        String file="";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please, introduce the name of the XML File you want to register.");
+        System.out.print("File: ");
+        file = in.nextLine();
+        System.out.println();
+        String uri=appPath+file;
         XMLReader fileReader = new XMLReader();
         fileReader.readFile(uri);
     }
