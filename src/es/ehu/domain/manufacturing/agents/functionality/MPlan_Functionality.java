ad3673452781a0ca02e9755e9e7b4e9e2a28632e
ekaitzhara <ekaitzhara16@gmail.com>
2020-09-28 11:06:51 +0200

    MPlan_Functionality seStart fixed

--- src/es/ehu/domain/manufacturing/agents/functionality/MPlan_Functionality.java
+++ src/es/ehu/domain/manufacturing/agents/functionality/MPlan_Functionality.java
@@ -51,33 +51,33 @@ public class MPlan_Functionality implements BasicFunctionality, AvailabilityFunc
     attributes.put("seClass", "es.ehu.domain.manufacturing.agents.OrderAgent");
 
     String status = seStart(myAgent.getLocalName(), attributes, null);
-
     if (status == null)
       System.out.println("OrderAgent created");
     else if (status == "-1")
-      System.out.println("ERROR creating OrderAgent");
+      System.out.println("ERROR creating OrderAgent -> No existe el ID del plan");
     else if (status == "-4")
-      System.out.println("ERROR creating OrderAgent > No targets");
+      System.out.println("ERROR creating OrderAgent -> No targets");
 
     return null;
+
+    //TODO El planAgent tiene que esperar a la confirmacion de que todos sus hijos se han creado para notificar que pasa al estado running (o tracking si es una réplica)
   }
 
   @Override
   public String seStart(String seID, Hashtable<String, String> attribs, String conversationId){
 
+    // seID --> ID del mPlanAgent
     String parentQuery = "get " + seID + " attrib=parent";
-    System.out.println("ID agent > " + seID);
     ACLMessage reply = null;
 
     try {
       reply = sendCommand(parentQuery);
       String planID = reply.getContent();
+      // ID del plan con el cual el agente está relacionado
       if (planID == null)   // Si no existe el id en el registro devuelve error
         return "-1";
-      System.out.println("PlanID: " + planID);  // ID del plan con el cual el agente está relacionado
 
-      String query = "get * parent="+ planID;
-      System.out.println("Query > " + query);
+      String query = "get order* parent="+ planID;  // Busco todos los order de los que el plan es parent (no se buscan todos los elementos porque pueden existir otros mPlanAgent en tracking)
       reply = sendCommand(query);
     } catch (Exception e) {
       e.printStackTrace();
@@ -86,63 +86,38 @@ public class MPlan_Functionality implements BasicFunctionality, AvailabilityFunc
     String allOrders = reply.getContent();
     if(allOrders == null)   // Si no existen orders en el registro devuelve error
       return "-1";
-    allOrders = allOrders.replace(seID + ",", "");
-    System.out.println("All orders> " + allOrders);
 
     List<String> items = Arrays.asList(allOrders.split("\\s*,\\s*"));
-    for (String order: items) {
-      System.out.println("Creating agent for order > " + order);
-      //StringBuilder startOrder = new StringBuilder("sestart "+ order);
-
+    for (String orderID: items) {
+      // Creamos los agentes para cada order
       try {
         String redundancy = "1";
         if ((attribs!=null) && (attribs.containsKey("redundancy")))
           redundancy = attribs.get("redundancy");
 
-        reply = sendCommand("get (get * parent=(get * parent=" + seID + " category=restrictionList)) attrib=attribValue");
+        reply = sendCommand("get (get * parent=(get * parent=" + orderID + " category=restrictionList)) attrib=attribValue");
         String refServID = reply.getContent();
-
 
         reply = sendCommand("get * category=pNodeAgent" + ((refServID.length()>0)?" refServID=" + refServID:""));
         String targets = reply.getContent();
         if (targets.length()<=0)
           return "-4";
 
-        reply = sendCommand("get " + seID + " attrib=category");
+        reply = sendCommand("get " + orderID + " attrib=category");
         String seCategory = reply.getContent();
         String seClass = attribs.get("seClass");
 
-
-        System.out.println("\tATRIBUTOS: \n\t\tORDER:" + order + "\n\t\tREDUNDANCY:" + redundancy
-                + "\n\t\trefServID:" + refServID + "\n\t\tTARGETS:" + targets + "\n\t\tCategory:" + seCategory
-                + "\n\t\tClass:" + seClass);
-
         // Orden de negociacion a todos los nodos
-        for (int i=0; i<Integer.parseInt(redundancy); i++) {
-          reply = sendCommand("localneg " + targets + " action=start "+seID+" criterion=max mem externaldata=" + seID + "," + seCategory + "," + seClass + "," + ((i==0)?"running":"tracking"));
-          //String neg = reply.getContent();
-
-        }
+        for (int i=0; i<Integer.parseInt(redundancy); i++)
+          reply = sendCommand("localneg " + targets + " action=start " + orderID + " criterion=max mem externaldata=" + orderID + "," + seCategory + "," + seClass + "," + ((i==0)?"running":"tracking"));
 
       } catch (Exception e) {
         e.printStackTrace();
       }
-
-      /*
-      if (attribs!=null)
-        attribs.entrySet().stream().forEach(entry -> startOrder.append(" " + entry.getKey() + "=" + entry.getValue()));
-
-      try {
-        reply = sendCommand(startOrder.toString());
-        System.out.println("Start order> " + reply.getContent());
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-      */
 
     }
 
-    return null;  //TODO
+    return null;
   }
 
   @Override
