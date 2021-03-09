3c8320ddb0a04c578e618d4ce0d5bbbea037f22c
alopez244 <alejandro.lopez@ehu.eus>
2021-03-09 20:28:25 +0100

    09/03/21 changes for the rpis

--- 01_gui_test.cmd
+++ 01_gui_test.cmd
@@ -1,1 +1,1 @@
-java -Dlog4j.configurationFile="log4j2.xml" -cp ".\lib\jade;.\lib\log4j\log4j-api-2.3.jar;.\lib\log4j\log4j-core-2.3.jar" jade.Boot -gui -host 192.168.0.250
+java -Dlog4j.configurationFile="log4j2.xml" -cp ".\lib\jade;.\lib\log4j\log4j-api-2.3.jar;.\lib\log4j\log4j-core-2.3.jar" jade.Boot -gui -host 192.168.0.250 -local-host 192.168.0.250 -port 1099 -local-port 1099
