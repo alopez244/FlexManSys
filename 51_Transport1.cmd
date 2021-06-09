dfa7aa9df597ceee6cb89e069a8c8e70a21182e8
alopez244 <alejandro.lopez@ehu.eus>
2021-06-09 09:37:37 +0200

    09/06/2021 Transport cmds have been updated.

--- 51_Transport1.cmd
+++ 51_Transport1.cmd
@@ -1,5 +1,4 @@
-java -Dlog4j.configurationFile="log4j2.xml" -cp ".\lib\jade;.\lib\log4j\log4j-api-2.3.jar;.\lib\log4j\log4j-core-2.3.jar;.\lib\commons\commons-codec-1.3.jar;.\lib\Gson\gson-2.8.6.jar;.\lib\commons\commons-io-2.6.jar;.\lib\commons\commons-lang3-3.8.1.jar;.\classes" jade.Boot -container auxma-local:es.ehu.domain.manufacturing.agents.TransportAgent(T_01)
+java -Dlog4j.configurationFile="log4j2.xml" -cp ".\lib\jade;.\lib\log4j\log4j-api-2.3.jar;.\lib\log4j\log4j-core-2.3.jar;.\lib\commons\commons-codec-1.3.jar;.\lib\Gson\gson-2.8.6.jar;.\lib\commons\commons-io-2.6.jar;.\lib\commons\commons-lang3-3.8.1.jar;.\classes" jade.Boot -container auxma-local:es.ehu.domain.manufacturing.agents.TransportAgent("T_01","12","10","50")
 
 
 
-java -Dlog4j.configurationFile="log4j2.xml" -cp "./lib/jade:/lib/log4j/log4j-api-2.3.jar:/lib/log4j/log4j-core-2.3.jar:/lib/commons/commons-codec-1.3.jar:/lib/Gson/gson-2.8.6.jar:/lib/commons/commons-io-2.6.jar:/lib/commons/commons-lang3-3.8.1.jar:/classes" jade.Boot -container auxma-local:es.ehu.domain.manufacturing.agents.TransportAgent T_01 12 10 50
