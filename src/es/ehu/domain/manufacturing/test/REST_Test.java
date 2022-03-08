7b7e44cb10e1f3394a9d17ea1392e3c03e780e8f
alopez244 <alejandro.lopez@ehu.eus>
2022-03-08 12:41:44 +0000

    08-03-22 Se ha creado la nueva rama Demostrador_IPB. Se ha añadido la librería Unirest y se están empezando a hacer pequeñas pruebas.

---
+++ src/es/ehu/domain/manufacturing/test/REST_Test.java
@@ -0,0 +1,24 @@
+package es.ehu.domain.manufacturing.test;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.HttpRequest;
+
+public class REST_Test {
+    public static void main(String[] args) {
+
+        // Esto es una primera prueba
+//        try {
+//            HttpResponse<JsonNode> jsonResponse = Unirest.post("").header("","").asJson();
+//        } catch (UnirestException e) {
+//            e.printStackTrace();
+//        }
+//        HttpRequest request = Unirest.get("");
+
+        // Ahora voy a hacer una prueba con alguno de los GET y con el POST del reset.
+
+
+    }
+}
