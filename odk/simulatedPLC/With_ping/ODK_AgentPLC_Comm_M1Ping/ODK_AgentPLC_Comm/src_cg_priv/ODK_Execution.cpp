188cdc14e7ce2a1bba63dc1d969b017fbf644e4b
alopez244 <alejandro.lopez@ehu.eus>
2022-03-10 13:48:40 +0000

    10-03-22 Se han incluido al repositorio las versiones de aplicación ODK editadas por Diego para comprobar cómo hace el check del estado del PLC.

---
+++ odk/simulatedPLC/With_ping/ODK_AgentPLC_Comm_M1Ping/ODK_AgentPLC_Comm/src_cg_priv/ODK_Execution.cpp
@@ -0,0 +1,240 @@
+/*
+ * This file is AUTO GENERATED - DO NOT MODIFY this file. 
+ * This file contains the execute function and trace helpers for ODK 1500S.
+ *
+ * File created by ODK_CodeGenerator version 205.100.101.18
+ * at Tue March 8 10:01:03 2022
+*/
+
+#include "ODK_Functions.h"
+#include "../src_odk_helpers/ODK_StringHelper.h"
+#include <string.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+#include "ODK_CpuReadData.h"
+#include "ODK_CpuReadWriteData.h"
+
+#define ODK_COMMAND_NOT_IMPLEMENTED    0x8098 
+#define MAX_LOG_ENTRY_LEN 				126
+#define MAX_USER_TRACE_LEN				(100)
+typedef struct TraceEntry_s
+{
+	ODK_UINT64  TickCounter;
+	ODK_UINT16  OB_Number; 
+	char        Text[MAX_USER_TRACE_LEN];
+} TraceEntry_t;
+
+#define ODK_TRACE_GET_TICK() __rdtsc()
+#ifdef ODK_TRACE_ON
+    #define MAX_NUMBER_OF_LOG_ENTRIES		(1 << 8) // must be 2^x (here 2^8=256)
+    TraceEntry_t g_TraceBuffer[MAX_NUMBER_OF_LOG_ENTRIES] = {0, 0, {0}};
+#else
+	#define MAX_NUMBER_OF_LOG_ENTRIES		(1) // must be 1 trace only
+	TraceEntry_t g_TraceBuffer[MAX_NUMBER_OF_LOG_ENTRIES] = {0, 0, "<trace off>"};
+#endif 
+EXPORT_API volatile unsigned long g_idxNextFreeEntry = 0;
+EXPORT_API volatile unsigned short g_currentOBnumber = 0;
+EXPORT_API ODK_UINT64 g_tickResolutionPerSec = (ODK_UINT64) 1u;
+EXPORT_API ODK_UINT64 g_tickTimeBase = 0;
+EXPORT_API ODK_UINT64 g_TimeBaseSec = 0;
+EXPORT_API ODK_UINT32 g_TimeBaseNanos = 0;
+EXPORT_API ODK_UINT32 g_ODK1500sBuildVersion = (2 << 24) + (5 << 16) + (1 << 8) + 0;
+EXPORT_API ODK_UINT8  g_SyncCallParallelCount = 3;
+EXPORT_API ODK_UINT32 g_SyncCallStackSize = (32 * 1024);
+EXPORT_API ODK_UINT32 g_SyncCallDataSize = (32 * 1024);
+EXPORT_API ODK_UINT32 g_PostMortemExceptionNr = ~0;   // 0=division by zero, 14=page fault (see https://en.wikipedia.org/wiki/Interrupt_descriptor_table)
+
+ // first if
+
+// declaration of the callbacks for testing them in Execute()
+EXPORT_API ODK_RESULT OnLoad (void);
+EXPORT_API ODK_RESULT OnUnload (void);
+EXPORT_API ODK_RESULT OnRun (void);
+EXPORT_API ODK_RESULT OnStop (void);
+
+//command enums
+typedef enum CommandHash_e
+{
+  FCT_HASH_initAgent = 0xEF7DB504,
+  FCT_HASH_SampleWrite = 0xFB6AE7D1,
+  FCT_HASH_SampleRead = 0x0B0BE76B,
+  FCT_HASH_sendConf = 0x29FAF677,
+  FCT_HASH_returnstate = 0x4404BB63,
+  FCT_HASH_GetTrace = 0xF06B562B
+}CommandHash_t;
+
+ODK_RESULT Execute ( const ODK_UINT32 cmd
+                              , const char* const in
+                              , const char*       inout
+                              , const char*       out)
+{
+  switch (cmd)
+  {
+    case FCT_HASH_initAgent:
+    {
+      return initAgent ();
+    }
+    case FCT_HASH_SampleWrite:
+    {
+      return SampleWrite (*((plc2agent*) &(in[0])), *((control_flags*) &(inout[0])));
+    }
+    case FCT_HASH_SampleRead:
+    {
+      return SampleRead (*((agent2plc*) &(out[0])), *((ODK_BOOL*) &(out[21])), *((ODK_BOOL*) &(out[22])), *((control_flags*) &(inout[0])));
+    }
+    case FCT_HASH_sendConf:
+    {
+      return sendConf ();
+    }
+    case FCT_HASH_returnstate:
+    {
+      return returnstate (*((ODK_BOOL*) &(in[0])), *((ODK_BOOL*) &(in[1])));
+    }
+    case FCT_HASH_GetTrace:
+    {
+      for (int i_1=0;i_1<256;i_1++)
+      {
+        *((ODK_S7STRING*) &(out[0 + (i_1*127)] )) = 125;
+      }
+      return GetTrace (*((ODK_INT16*) &(in[0])), (ODK_S7STRING(*)[127]) &(out[0]));
+    }
+    default:
+    {
+      typedef ODK_RESULT (*t_CallbackFunction)(void);
+      #pragma warning ( disable: 4189 )
+      // check the existence of the callback functions
+      // (but suppress warning of not used variables)
+      {
+        t_CallbackFunction tmp1 = &OnLoad;
+        t_CallbackFunction tmp2 = &OnUnload;
+        t_CallbackFunction tmp3 = &OnRun;
+        t_CallbackFunction tmp4 = &OnStop;
+      }
+      return ODK_COMMAND_NOT_IMPLEMENTED;
+    }
+  }
+}
+
+typedef struct Internal_ExecuteRT_s
+{
+  ODK_UINT32    Command;
+  char*        _IN;
+  char*        _INOUT;
+  char*        _OUT;
+} Internal_ExecuteRT_t;
+
+EXPORT_API int ExecuteRT(void* p_pMsg)
+{
+  Internal_ExecuteRT_t* pMsg = (Internal_ExecuteRT_t*) p_pMsg;
+  return Execute (pMsg->Command, pMsg->_IN, pMsg->_INOUT, pMsg->_OUT);
+}
+
+void ODK_Trace(const char* p_szFileName, const int p_nLine, const char* p_szMsg, ...)
+{
+#ifdef ODK_TRACE_ON
+
+  char szLog[MAX_USER_TRACE_LEN];
+
+  // get message
+  va_list	args;
+  va_start (args, p_szMsg);
+  vsnprintf (szLog, MAX_USER_TRACE_LEN, p_szMsg, args);
+  va_end (args);
+  szLog[MAX_USER_TRACE_LEN - 1] = 0;
+  // memcpy to trace buffer in case of realtime
+
+  // always increment the index, but mask out the high bits
+  unsigned long currentIdx = InterlockedExchangeAdd(&g_idxNextFreeEntry, 1)
+                             & (MAX_NUMBER_OF_LOG_ENTRIES - 1);
+
+  ODK_UINT64 time;
+  time = ODK_TRACE_GET_TICK();
+
+  g_TraceBuffer[currentIdx].OB_Number = g_currentOBnumber;
+  g_TraceBuffer[currentIdx].TickCounter = time;
+  #pragma warning(suppress: 6031)
+  snprintf((char*)&(g_TraceBuffer[currentIdx].Text[0]), MAX_USER_TRACE_LEN, "%s(%d): %s", p_szFileName, p_nLine, szLog);
+  g_TraceBuffer[currentIdx].Text[MAX_USER_TRACE_LEN - 1] = 0;
+#endif // ODK_TRACE_ON
+}
+
+int GetTraceTime(const ODK_UINT64* p_pTicks, char* p_str, const int p_maxLen)
+{
+  ODK_UINT64 tickDiff = *p_pTicks - g_tickTimeBase;
+  ODK_UINT64 seconds = g_TimeBaseSec + (tickDiff / g_tickResolutionPerSec);
+  ODK_UINT64 nanosecDiff = ((tickDiff % g_tickResolutionPerSec) * 1000000000L) / g_tickResolutionPerSec;
+  ODK_UINT32 microsec = (g_TimeBaseNanos + (ODK_UINT32) nanosecDiff) / 1000;
+  
+  if (1000000 <= microsec)
+  {
+    seconds++;
+    microsec -= 1000000;
+  }
+  time_t* pGmTime = (time_t*) &seconds;
+  struct tm* pTraceTime = gmtime(pGmTime);
+  
+  if (p_maxLen >= 32)
+  {
+    return sprintf(p_str, "%d/%02d/%02d %.2d:%.2d:%.2d.%.6lu"
+                 , pTraceTime->tm_year + 1900, pTraceTime->tm_mon + 1, pTraceTime->tm_mday
+                 , pTraceTime->tm_hour, pTraceTime->tm_min, pTraceTime->tm_sec, microsec);
+  }
+  
+  p_str[0] = 0;
+  return 0;
+}
+
+ODK_RESULT GetTrace (const ODK_INT16& TraceCount, ODK_S7STRING TraceBuffer[256][127]) 
+{
+  ODK_UINT32 myIdx = g_idxNextFreeEntry - 1;
+  ODK_INT16  myTraceCount = TraceCount;
+  int filled = 0;
+
+  if (myTraceCount > MAX_NUMBER_OF_LOG_ENTRIES ||  myTraceCount <= 0)
+  {
+	myTraceCount = MAX_NUMBER_OF_LOG_ENTRIES;
+  }
+  int _odk_internal_i = 0;
+  // copy TraceCount trace entries
+  for (; _odk_internal_i < myTraceCount; _odk_internal_i++)
+  {
+    char str[MAX_LOG_ENTRY_LEN] = "";
+    int index = myIdx & (MAX_NUMBER_OF_LOG_ENTRIES - 1);
+    #ifdef ODK_TRACE_ON
+      if(g_TraceBuffer[index].TickCounter != 0)
+      {
+         filled = GetTraceTime(&(g_TraceBuffer[index].TickCounter), str, MAX_LOG_ENTRY_LEN);
+      }
+      else
+      {
+        // if tickcount is zero, do not write anything to buffer
+        filled = MAX_LOG_ENTRY_LEN;
+      }
+    #endif
+	#pragma warning(suppress: 6031)
+	snprintf(&(str[filled]), MAX_LOG_ENTRY_LEN - (int64_t)filled, " OB%d %s", g_TraceBuffer[index].OB_Number, g_TraceBuffer[index].Text);
+	str[MAX_LOG_ENTRY_LEN - 1] = 0;
+    Convert_SZSTR_to_S7STRING (str, TraceBuffer[_odk_internal_i]);
+    myIdx--;
+    filled = 0;
+  }
+  // fill empty strings for not wished trace entries
+  for (; _odk_internal_i < 256; _odk_internal_i++)
+  {
+    Convert_SZSTR_to_S7STRING ("", TraceBuffer[_odk_internal_i]);
+    myIdx--;
+  }
+
+  return myTraceCount;
+}
+
+// function for call from SFx (exception)
+EXPORT_API void ODK_Trace_00(const char* p_szMsg)
+{
+  #ifdef ODK_TRACE_ON
+  ODK_Trace("<unknown>", 0, p_szMsg);
+  #endif
+}
---
+++ odk/simulatedPLC/With_ping/ODK_AgentPLC_Comm_M5Ping/ODK_AgentPLC_Comm/ODK_AgentPLC_Comm.cpp
@@ -0,0 +1,357 @@
+#include "stdafx.h"
+#include "ODK_Functions.h"
+#include "ODK_StringHelper.h"
+#include "tchar.h"
+
+#include <jni.h>
+#include "userDefines.h"
+#include <string>
+#include "nlohmann\json.hpp"
+using json = nlohmann::json;
+
+// Include MQTT library
+//#include "include/MQTTClient.h"
+
+//MQTTClient client;
+
+/*
+ * OnLoad() is invoked after the application binary was loaded.
+ * @return ODK_SUCCESS       notify, that no error occurs
+ *                            - OnRun() will be invoked automatically
+ *         any other value   notify, that an error occurs (user defined)
+ *                            - OnUnload() will be invoked automatically
+ *                            - ODK application will be unloaded automatically
+ */
+EXPORT_API ODK_RESULT OnLoad(void)
+{
+	// place your code here
+	return ODK_SUCCESS;
+}
+
+/*
+ * OnUnload() is invoked before the application binary is unloaded or when
+ *            ODK-Host terminates.
+ * @return ODK_SUCCESS       notify, that no error occurs
+ *         any other value   notify, that an error occurs (user defined)
+ *                            - ODK application will be unloaded anyway
+ */
+EXPORT_API ODK_RESULT OnUnload(void)
+{
+	// place your code here
+	return ODK_SUCCESS;
+}
+
+/*
+ * OnRun() is invoked when the CPU transitions to the RUN state and
+ *         after OnLoad().
+ * @return Does not affect the load operation or state transition.
+ */
+EXPORT_API ODK_RESULT OnRun(void)
+{
+	// place your code here
+	return ODK_SUCCESS;
+}
+
+/*
+ * OnStop() is invoked when the CPU transitions to the STOP/SHUTDOWN state
+ *          or before unloading the ODK application.
+ * @return Does not affect the load operation or state transition.
+ */
+EXPORT_API ODK_RESULT OnStop(void)
+{
+	// place your code here
+	return ODK_SUCCESS;
+}
+
+
+std::string transformTimeStamp(unsigned long long TimeStamp)
+{
+
+	/*Primero pasamos de nanosegundos a segundos*/
+	unsigned long long seconds = TimeStamp / 1000000000;
+
+	time_t date = seconds;
+
+	tm* ltm = localtime(&date);
+
+	std::string value;
+	value = std::to_string(1900 + ltm->tm_year); value += "/"; value += std::to_string(1 + ltm->tm_mon); value += "/"; value += std::to_string(ltm->tm_mday); value += " "; value += std::to_string(ltm->tm_hour); value += ":"; value += std::to_string(ltm->tm_min); value += ":"; value += std::to_string(ltm->tm_sec);
+	return value;
+}
+/*
+
+void connectWithBroker() {
+	//Aquí creamos la conexión con el broker MQTT
+	char SERVER_ADDRESS[256] = "192.168.2.240:31883";
+	char CLIENT_ID[256] = "odk_1";
+	char TOPIC[256] = "1";
+
+	MQTTClient_create(&client, SERVER_ADDRESS, CLIENT_ID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+	MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+
+	conn_opts.username = "admin";
+	conn_opts.password = "mosquittoGCIS";
+
+	int rc;
+	if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
+		//debugFile << "\n Failed to connect, return code %d\n";
+	}
+	//debugFile << "\n Connection successful!";
+
+	//Aquí cierro el fichero de debugueo
+	//debugFile.close();
+}
+
+void publishData(char* TOPIC, char* payload) {
+	//char TOPIC[256] = topic;
+
+	MQTTClient_message pubmsg = MQTTClient_message_initializer;
+	pubmsg.payload = payload;
+	pubmsg.payloadlen = strlen(payload);
+	pubmsg.qos = 0;
+	pubmsg.retained = 0;
+	MQTTClient_deliveryToken token;
+	MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+	MQTTClient_waitForCompletion(client, token, 1000L);
+
+}
+*/
+ODK_RESULT initAgent() {
+	static bool uniqueStart = false;	//Variable to avoid multiple initializations
+	if (!uniqueStart) {				//It only happens on the first run
+		uniqueStart = true;			//Update the flag to prevent it from being run again
+		if (!JNIinit())	return ODK_USER_ERROR_BASE;
+		jstring machineID = env->NewStringUTF("5");	//Se inicializa como agente gateway para la maquina 5 
+		env->CallStaticVoidMethod(cls, JNI_init, machineID);	//Executes the init method using JNI
+		if (env->ExceptionOccurred())
+		{
+			env->ExceptionDescribe();
+		}
+	}
+	return ODK_SUCCESS;
+}
+
+//Parses a String in JSON format and leave its data in a structure
+ODK_RESULT SampleRead(/*OUT*/agent2plc& str_in,	/*OUT*/ODK_BOOL& tRecv, /*OUT*/ODK_BOOL& tData, /*INOUT*/control_flags& flags) {
+
+	char msgInCa[254] = { 0 };		//An array is created to reserve that memory
+	const char* msgInC = msgInCa;	//Pointer to array of characters is created
+									//Executes the recv method through JNI and receives a jstring with the message to read
+	jstring s = (jstring)env->CallStaticObjectMethod(cls, JNI_recv);
+	if (env->ExceptionOccurred())
+	{
+		env->ExceptionDescribe();
+	}
+	msgInC = env->GetStringUTFChars(s, 0);					//Type conversion jstring->char*
+
+	uint16_t ret;							//Variable to store the result of the function
+	tRecv = false;
+	tData = false;
+	json j = json::parse(msgInC);			//The String is parsed and converted to a JSON object
+	if (j.contains("Received")) {			//It is checked if it is a receipt confirmation
+		if (j["Received"] == true) {
+			tRecv = true;
+			ret = ODK_SUCCESS;
+		}
+	}
+	else if (j.contains("Control_Flag_New_Service")) {	//At least the first field is checked
+		tData = true;
+		//The received parameters are copied to the output structure		
+		flags.Control_Flag_New_Service = j["Control_Flag_New_Service"].get<bool>();
+		flags.Control_Flag_Item_Completed = false;
+		flags.Control_Flag_Service_Completed = false;
+		str_in.Id_Machine_Reference = j["Id_Machine_Reference"].get<int>();
+		str_in.Id_Order_Reference = j["Id_Order_Reference"].get<int>();
+		str_in.Id_Batch_Reference = j["Id_Batch_Reference"].get<int>();
+		str_in.Id_Ref_Subproduct_Type = j["Id_Ref_Subproduct_Type"].get<int>();
+		str_in.Operation_Ref_Service_Type = j["Operation_Ref_Service_Type"].get<int>();
+
+		str_in.Operation_No_of_Items = j["Operation_No_of_Items"].get<int>();
+
+		ret = ODK_SUCCESS;
+	}
+	return ret;
+}
+
+//From a data structure a String is generated in JSON format
+ODK_RESULT SampleWrite(/*IN*/const plc2agent& str_out, /*INOUT*/ control_flags& flags) {
+	json j;	//JSON Object
+			//Each element of the structure is copied into a Json object
+	j["Control_Flag_Item_Completed"] = (bool)flags.Control_Flag_Item_Completed;
+	j["Control_Flag_Service_Completed"] = (bool)flags.Control_Flag_Service_Completed;
+	j["Id_Machine_Reference"] = (uint32_t)str_out.Id_Machine_Reference;
+	j["Id_Order_Reference"] = (uint32_t)str_out.Id_Order_Reference;
+	j["Id_Batch_Reference"] = (uint32_t)str_out.Id_Batch_Reference;
+	j["Id_Ref_Subproduct_Type"] = (uint32_t)str_out.Id_Ref_Subproduct_Type;
+	j["Id_Ref_Service_Type"] = (uint32_t)str_out.Id_Ref_Service_Type;
+	j["Id_Item_Number"] = (uint8_t)str_out.Id_Item_Number;
+
+	unsigned long long Data_Initial_Time_Stamp_T = str_out.Data_Initial_Time_Stamp;
+	unsigned long long Data_Final_Time_Stamp_T = str_out.Data_Final_Time_Stamp;
+	unsigned long long Data_Service_Time_Stamp_T = str_out.Data_Service_Time_Stamp;
+
+	j["Data_Initial_Time_Stamp"] = transformTimeStamp(Data_Initial_Time_Stamp_T);
+	j["Data_Final_Time_Stamp"] = transformTimeStamp(Data_Final_Time_Stamp_T);
+	j["Data_Service_Time_Stamp"] = transformTimeStamp(Data_Service_Time_Stamp_T);
+
+	//Preparar formato para broker MQTT
+	std::string machineId;
+	std::string totalPayload;
+
+	machineId = std::to_string(str_out.Id_Machine_Reference);
+	totalPayload = "Id_Machine_Reference=" + std::to_string(str_out.Id_Machine_Reference);
+	totalPayload = totalPayload + "&Id_Order_Reference=" + std::to_string(str_out.Id_Order_Reference);
+	totalPayload = totalPayload + "&Id_Batch_Reference=" + std::to_string(str_out.Id_Batch_Reference);
+	totalPayload = totalPayload + "&Id_Ref_Subproduct_Type=" + std::to_string(str_out.Id_Ref_Subproduct_Type);
+	totalPayload = totalPayload + "&Id_Ref_Service_Type=" + std::to_string(str_out.Id_Ref_Service_Type);
+	totalPayload = totalPayload + "&Id_Item_Number=" + std::to_string(str_out.Id_Item_Number);
+	totalPayload = totalPayload + "&Data_Initial_Time_Stamp=" + transformTimeStamp(Data_Initial_Time_Stamp_T);
+	totalPayload = totalPayload + "&Data_Final_Time_Stamp=" + transformTimeStamp(Data_Final_Time_Stamp_T);
+
+	if (flags.Control_Flag_Service_Completed == true) {
+		totalPayload = totalPayload + "&Data_Service_Time_Stamp=" + transformTimeStamp(Data_Service_Time_Stamp_T);
+	}
+    //Inicio de MQTT se comenta
+	// Nos conectamos al broker
+	/*
+	connectWithBroker();
+
+	// Una vez añadidos todos los datos, los subiremos al broker via MQTT
+	char* charPayload = (char*)totalPayload.c_str();
+	char* charMachineId = (char*)machineId.c_str();
+
+	publishData(charMachineId, charPayload);
+	*/
+	std::string msgJson = j.dump(); //json serialized in c++ string data type
+	jstring msg = env->NewStringUTF(msgJson.c_str());	//Type conversion using UTF-8 string -> jstring
+	env->CallStaticVoidMethod(cls, JNI_send, msg);	//The send function is executed through JNI
+	if (env->ExceptionOccurred())
+	{
+		env->ExceptionDescribe();
+	}
+
+	return ODK_SUCCESS;
+}
+
+//Generates a String in JSON format with the message receipt confirmation
+ODK_RESULT sendConf() {
+	json j;							//JSON Object
+	j["Received"] = true;			//The "Received" key is added to the object
+	char JsonCa[256 + 1] = { 0 };		//An array is generated to reserve that memory
+	char* JsonC = JsonCa;			//A pointer is created that points to the created array
+	sprintf(JsonC, "%s", (j.dump()).c_str());	//Serialized->String->const char*
+	jstring msg = env->NewStringUTF(JsonC);	//Type conversion char* -> jstring
+	env->CallStaticVoidMethod(cls, JNI_send, msg);	//The send function is executed through JNI
+	if (env->ExceptionOccurred())
+	{
+		env->ExceptionDescribe();
+	}
+	return ODK_SUCCESS;
+}
+
+
+ODK_RESULT returnstate(const ODK_BOOL& pedidoactivo, const ODK_BOOL& detectederror) {
+	bool ask = (bool)env->CallStaticBooleanMethod(cls, JNI_askstate);
+	if (ask == true) {
+	if (pedidoactivo == true && detectederror == false) {
+		jstring state = env->NewStringUTF("Working");
+		env->CallStaticVoidMethod(cls, JNI_rcvstate, state);
+	}
+	else if (pedidoactivo == false && detectederror == false) {
+		jstring state = env->NewStringUTF("Not working");
+		env->CallStaticVoidMethod(cls, JNI_rcvstate, state);
+	}
+	else if (pedidoactivo == true && detectederror == true) {
+		jstring state = env->NewStringUTF("Error while working");
+		env->CallStaticVoidMethod(cls, JNI_rcvstate, state);
+	}
+	else if (pedidoactivo == false && detectederror == true) {
+		jstring state = env->NewStringUTF("Error while not working");
+		env->CallStaticVoidMethod(cls, JNI_rcvstate, state);
+	}
+	else {
+		jstring state = env->NewStringUTF("Unknown");
+		env->CallStaticVoidMethod(cls, JNI_rcvstate, state);
+	}
+
+}
+	while (ask == true) {
+		ask = (bool)env->CallStaticBooleanMethod(cls, JNI_askstate);
+	}
+
+	if (env->ExceptionOccurred())
+	{
+		env->ExceptionDescribe();
+	}
+	return ODK_SUCCESS;
+}
+
+
+
+
+bool JNIinit(void) {
+	ODK_TRACE("-->JNIinit");
+	//The javaVM parameters are assigned and initialized
+
+	JavaVMOption options[1];
+	JavaVMInitArgs vm_args;	
+
+	typedef jint(JNICALL* pCreateJavaVM)(JavaVM**, void**, void*);
+	HINSTANCE hInstance = LoadLibrary(L"C:\\Program Files (x86)\\Java\\jre1.8.0_271\\bin\\client\\jvm.dll");
+	pCreateJavaVM CreateJavaVM = (pCreateJavaVM)GetProcAddress(hInstance, "JNI_CreateJavaVM");
+
+	//options[0].optionString = "-Djava.class.path=C:\\Users\\aabadia004\\IdeaProjects\\FlexManSys\\classes;C:\\Users\\aabadia004\\Documents\\java\\libraries\\jade\\lib\\jade.jar;C:\\Users\\aabadia004\\Documents\\Java\\libraries\\gson\\gson-2.8.6.jar;C:\\Users\\aabadia004\\Documents\\Java\\libraries\\commons collections\\commons-collections4-4.4.jar";
+	//options[0].optionString = "-Djava.class.path=C:\\Users\\dgarcia129\\IdeaProjects\\FlexManSys\\classes;C:\\Users\\dgarcia129\\Documents\\Java\\Libraries\\jade-4.3.jar;C:\\Users\\dgarcia129\\Documents\\Java\\Libraries\\gson-2.8.6.jar;C:\\Users\\dgarcia129\\Documents\\Java\\Libraries\\commons-collections4-4.4.jar";
+	options[0].optionString = "-Djava.class.path=C:\\FlexManSys\\classes;C:\\Users\\dgarcia129\\Documents\\Java\\Libraries\\jade-4.3.jar;C:\\Users\\dgarcia129\\Documents\\Java\\Libraries\\gson-2.8.6.jar;C:\\Users\\dgarcia129\\Documents\\Java\\Libraries\\commons-collections4-4.4.jar";
+	memset(&vm_args, 0, sizeof(vm_args));
+	vm_args.version = JNI_VERSION_1_6;
+	vm_args.nOptions = 1;
+	vm_args.options = options;
+	vm_args.ignoreUnrecognized = JNI_TRUE;
+	//long status = JNI_CreateJavaVM(&jvm, (void**)&env, &vm_args);
+	long status = CreateJavaVM(&jvm, (void**)&env, &vm_args);
+
+	if (status == JNI_ERR) {
+		ODK_TRACE("--Error creating JavaVM");
+		return false;
+	}
+	//The class that contains the necessary functions is searched for
+	cls = env->FindClass("es/ehu/domain/manufacturing/agents/cognitive/ExternalJADEgw");
+	if (cls == NULL) {
+		ODK_TRACE("--Error finding the class");
+		return false;
+	}
+	//The static agentInit method is searched inside the class
+	JNI_init = env->GetStaticMethodID(cls, "agentInit", "(Ljava/lang/String;)V");		//javap -s -p <clase>
+	if (JNI_init == 0) {
+		ODK_TRACE("--Error finding method agentInit");
+		return false;
+	}
+	//The static send method is searched inside the class
+	JNI_send = env->GetStaticMethodID(cls, "send", "(Ljava/lang/String;)V");		//javap -s -p <clase>
+	if (JNI_send == 0) {
+		ODK_TRACE("--Error finding method send");
+		return false;
+	}
+	//The recv send method is searched inside the class
+	JNI_recv = env->GetStaticMethodID(cls, "recv", "()Ljava/lang/String;");		//javap -s -p <clase>
+	if (JNI_recv == 0) {
+		ODK_TRACE("--Error finding method recv");
+		return false;
+	}
+	
+	JNI_rcvstate = env->GetStaticMethodID(cls, "rcvstate", "(Ljava/lang/String;)V");		//javap -s -p <clase>
+	if (JNI_rcvstate == 0) {
+		ODK_TRACE("--Error finding method rcvstate");
+		return false;
+	}
+	JNI_askstate = env->GetStaticMethodID(cls, "askstate", "()Z");		//javap -s -p <clase>
+	if (JNI_askstate == 0) {
+		ODK_TRACE("--Error finding method askstate");
+		return false;
+	}
+
+	ODK_TRACE("<--JNIinit");
+	return true;
+}
+
